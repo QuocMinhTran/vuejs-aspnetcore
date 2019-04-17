5a2590b8ca3cdc0601dfd6507102478a11d49f13
MinhTran <minh.tran@brandoncompany.com>
2019-04-17 14:48:47 +0200

    First commit

---
+++ netcore-vuejs/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using netcore_vuejs.Models;
+
+namespace netcore_vuejs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SubmitedForm([FromBody] FormViewModel form)
+        {
+            string message = "";
+            // Insert to database, send email, etc
+            return Json(new { message });
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
